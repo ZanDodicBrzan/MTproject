e8b43a10126866dd2869256a3c6ef1587729f4ca
pickosir <fb.aljaz@gmail.com>
2021-01-11 14:37:59 +0100

    idk

--- js/index.js
+++ js/index.js
@@ -262,72 +262,6 @@ datumDo.addEventListener('change' , async(event) =>{
 
 // =============================================================================
 
-var tip = d3.select(".chart-container")
-	.append("div")
-  .attr("class", "tip")
-	.style("position", "absolute")
-	.style("z-index", "10")
-	.style("visibility", "hidden");
-
-var svg = d3.select("svg").attr("class", "background-style"),
-    margin = {top: 20, right: 20, bottom: 42, left: 40},
-    width = +svg.attr("width") - margin.left - margin.right,
-    height = +svg.attr("height") - margin.top - margin.bottom;
-
-var x = d3.scaleBand().rangeRound([0, width]).padding(0.05),
-    y = d3.scaleLinear().rangeRound([height, 0]);
-
-var g = svg.append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-    
-/*d3.json("apiPlaceholderURL", function(error, data) {
-  //if (error) throw error;
-
-  data = deathsPerAge;
-  
-  x.domain(data.map(function(d) { return d.age_group; }));
-  y.domain([0, d3.max(data, function(d) { return d.deaths; })]);
-
-  g.append("g")
-      .attr("class", "axis axis--x")
-      .attr("transform", "translate(0," + height + ")")
-      .call(d3.axisBottom(x))
-   .append("text")
-      .attr("y", 6)
-      .attr("dy", "2.5em")
-      .attr("dx", width/2 - margin.left)
-      .attr("text-anchor", "start")
-      .text("Grad Year");
-
-  g.append("g")
-      .attr("class", "axis axis--y")
-      .call(d3.axisLeft(y).ticks(10))
-    .append("text")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 6)
-      .attr("dy", "0.71em")
-      .attr("text-anchor", "end")
-      .text("Student Count");
- 
-
-  g.selectAll(".bar")
-    .data(data)
-    .enter().append("rect")
-      .attr("class", "bar")
-      .attr("x", function(d) { return x(d.age_group); })
-      .attr("y", function(d) { return y(d.deaths); })
-      .attr("width", x.bandwidth())
-      .attr("height", function(d) { return height - y(d.deaths)})
-      .on("mouseover", function(d) {return tip.text(d.deaths).style("visibility", "visible").style("top", y(d.deaths) - 13+ 'px' ).style("left", x(d.age_group) + x.bandwidth() - 12 + 'px')})
-	    //.on("mousemove", function(){return tooltip.style("top", (d3.event.pageY-10)+"px").style("left",(d3.event.pageX+10)+"px");})
-      .on("mouseout", function(){return tip.style("visibility", "hidden");});
-});
-*/
-
-
-
-
 const render = async() => {
 
   let allToDate = [];
@@ -436,7 +370,7 @@ const render = async() => {
     .attr("text-anchor", "middle")
     .style("font-size", "25px") 
     .text("Skupno število okužb po starostnih skupinah")
-    .style("font-weight", "bold")
+    .style("font-weigh", "bold")
     .attr("dy", "1em")
     .style("fill", "black");
 
@@ -451,16 +385,13 @@ const render = async() => {
 
   svgGraph.append("text")
     //.attr("transform", "rotate(-90)")
+    .attr("x", 10)
     .attr("y", 10)
-    .attr("x", (height / 2))
     .attr("dy", "1em")
     .style("text-anchor", "middle")
     .style("font-style", "italic")
     .text("Število okužb")
     .style("fill", "Black"); 
-
-  svg.node();
-
 };
 
 
